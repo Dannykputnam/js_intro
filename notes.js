d316913e3763db3b36bc2d0565c4cc0d03437ae2
Danny Putnam <dannykputnam@gmail.com>
2021-11-17 19:33:11 -0700

    added notes on var and ruby compare.

---
+++ notes.js
@@ -0,0 +1,41 @@
+// Datatypes
+// String "" ''
+// Boolean true false
+// integer
+// float
+// hash
+// {first_name: "bob", age: 2}
+// Array
+// [''.321321, []]
+// null 
+// NaN
+// undefined
+// = - / * %
+// () 
+// || && 
+// < > <= >= 
+// =
+// ==
+// ===
+// !=
+// !==
+// ++
+// --
+// +=
+// *=
+
+// variables 
+// var firstName = "bob"
+// - letters, nimbers, _ $
+// - has to begin with a leter $ _
+// - case sensitive 
+// - stay away from reserve words
+// No longer using var because of scope
+// var file wide scope
+// - Connecting two words together camel case
+// To call a varibale use 
+// let - things you want to change
+// const - Things that you want as constant
+// scope is wherever it is your defined it in.
+
+
